a5d1d3f3a65b8be83d87daf9771f4c3c6b01f690
Amandeep Kaur <amey0404@yahoo.com>
2022-11-19 00:01:59 -0500

    Add files via upload

--- stack_bkg_etau/stack_bkg_2017.C
+++ stack_bkg_etau/stack_bkg_2017.C
@@ -6,17 +6,17 @@ TCanvas *c1= new TCanvas("c1","stacked hists",0,0,600,600);
 TH1 *h[3], *hsignal[4], *hstat;
 Double_t bins[13] = {100,150,200,250,300,350,400,450,500,550,650,850, 1500};
 
-TFile *f1 = TFile::Open("fakebkg_etau_2016.root");
+TFile *f1 = TFile::Open("fakebkg_etau_2017.root");
 f1->GetObject("h1",h[0]);
 
-TFile *fstat = TFile::Open("fakebkg_etau_2016.root");
+TFile *fstat = TFile::Open("fakebkg_etau_2017.root");
 fstat->GetObject("h1",hstat);
 
-TFile *f2 = TFile::Open("ttbar_etau_2016.root");
+TFile *f2 = TFile::Open("ttbar_etau_2017.root");
 f2->GetObject("h1",h[1]);
 
-TFile *f3 = TFile::Open("prompt_etau_2016.root");
-f3->GetObject("h1",h[2]);
+TFile *f3 = TFile::Open("prompt_etau_2017.root");
+f3->GetObject("hist",h[2]);
 
 TFile *f4 = TFile::Open("VBF_Zprime_tau_tau_M_1000_gl_0_gh_1_kv_1p0.root");
 f4->GetObject("NDiJetCombinations/Electron1Tau1ReconstructableMass",hsignal[0]);
@@ -30,10 +30,10 @@ f6->GetObject("NDiJetCombinations/Electron1Tau1ReconstructableMass",hsignal[2]);
 TFile *f7 = TFile::Open("VBF_Zprime_WW_M_1250_gl_0_gh_1_kv_1p0.root");
 f7->GetObject("NDiJetCombinations/Electron1Tau1ReconstructableMass",hsignal[3]);
 
-h[0]->Scale(0.73/0.59);
-hstat->Scale(0.73/0.59);
+h[0]->Scale(0.71/0.74);
+hstat->Scale(0.71/0.74);
 
-double lumi = 35900.;
+double lumi = 41500.;
 for(int i=0; i<4; i++){
 double xsec[4] = {0.1994, 0.02174, 0.8273, 0.06114};
 double fac = (xsec[i]*lumi)/(100000);
@@ -84,7 +84,7 @@ hs->GetXaxis()->SetTitleOffset(1.1);
 c1->Update();
 c1->Modified();
 
-        double y_legend = hs->GetMaximum() + 140;
+        double y_legend = hs->GetMaximum() + 230;
         double x_legend = 170;
         TLatex *   tex = new TLatex(x_legend,y_legend,"CMS");
 //      TLatex *   tex = new TLatex(105,400,"CMS");
@@ -100,7 +100,7 @@ c1->Modified();
    tex1->Draw();
 
         double x_pos1 = x_legend+1050;
-        TLatex * tex2 = new TLatex(x_pos1,y_legend,"2016, 35.9 fb^{-1} (13 TeV)");
+        TLatex * tex2 = new TLatex(x_pos1,y_legend,"2017, 41.5 fb^{-1} (13 TeV)");
         tex2->SetTextAlign(20);
         tex2->SetTextFont(42);
         tex2->SetTextSize(0.0332915);
@@ -118,6 +118,6 @@ TLegend *legend=new TLegend(0.52,0.65,0.89,0.88);
 	legend->AddEntry(hstat,"Stat. Uncert.","fp");
 	legend->Draw();
 
-c1->SaveAs("bkg_etau_2016.root");
-c1->SaveAs("bkg_etau_2016.pdf");
+c1->SaveAs("bkg_etau_2017.root");
+c1->SaveAs("bkg_etau_2017.pdf");
 	}
